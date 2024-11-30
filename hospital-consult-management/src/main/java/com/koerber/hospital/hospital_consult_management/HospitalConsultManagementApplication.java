fb47b8e7442529bb5b7b0c692803ccf51fd3d57f
luis-c-fonseca <luis-c-fonseca@telecom.pt>
2024-11-30 17:24:53 +0000

    basic project structure creation

---
+++ hospital-consult-management/src/main/java/com/koerber/hospital/hospital_consult_management/HospitalConsultManagementApplication.java
@@ -0,0 +1,13 @@
+package com.koerber.hospital.hospital_consult_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalConsultManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalConsultManagementApplication.class, args);
+	}
+
+}
---
+++ hospital-consult-management/src/test/java/com/koerber/hospital/hospital_consult_management/HospitalConsultManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koerber.hospital.hospital_consult_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalConsultManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
